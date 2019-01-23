bd4e8343871e648831bd8166a717b55e33913bd1
parimalak <parimala.killada@rockets.utoledo.edu>
2019-01-23 10:23:15 -0500

    :recycle: Refactor the code for multiple consents

--- service.py
+++ service.py
@@ -2,7 +2,9 @@ from botocore.vendored import requests
 import os
 import boto3
 import json
-
+import xmltodict
+import copy
+from collections import defaultdict
 
 class DataserviceException(Exception):
     pass
@@ -11,53 +13,194 @@ class DataserviceException(Exception):
 class TimeoutException(Exception):
     pass
 
+record_template = {
+    "study": {
+        "dbgap_id": "phs001247"
+    }
+}
 
-def handler(event, context):
+bs_record_template = {
+    "biospecimen": {
+        "kf_id": "SD_9PYZAHHE"
+    }
+}
+
+def dict_or_list(key, dictionary):
+    if type(dictionary) != 'str':
+        for k, v in dictionary.items():
+            if k == key:
+                yield v
+            elif isinstance(v, dict):
+                for result in dict_or_list(key, v):
+                    yield result
+            elif isinstance(v, list):
+                for d in v:
+                    if isinstance(d, dict):
+                        for result in dict_or_list(key, d):
+                            yield result
+
+def read_dbgap_xml(accession):
+    """
+    Reads db_gap xml file and fetches consent code and external sample id
+    for a given study
+    :returns: A list of tuples (consent_code, sample_id, consent_name)
+        for each sample in the study.
+    """
+    url = (f'https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/' +
+           f'GetSampleStatus.cgi?study_id={accession}&rettype=xml')
+    data = requests.get(url)
+    if data.status_code != 200:
+        raise DbGapException(f'Request for study {accession} returned non-200 '
+                             f'status code: {data.status_code}')
+
+    data = xmltodict.parse(data.content)
+    study_status = list(dict_or_list('@registration_status', data))
+
+    if study_status[0] in ['released']:
+        dbgap_codes = zip(dict_or_list('@consent_code', data),
+                          dict_or_list('@submitted_sample_id', data),
+                          dict_or_list('@consent_short_name', data))
+        return dbgap_codes
+    else:
+        raise DbGapException(f'study {accession} is not released by dbgap. '
+                             f'registration_status: {study_status[0]}')
+
+def map_one_study(study, dataservice_api):
+    """
+    Attempt to load a dbGaP xml for a study and call a function for each
+    sample to update
+    :param study: The dbGaP study_id
+    :param lam: A boto lambda client used to invoke lamda functions
+    :param consentcode: The name of the function that will be called for each
+        sample to update it inside the dataservice
+    :param dataservice_api: The url of the dataservice api
+    """
+    # Get dbgap released version from dataservice
+    url = f'{dataservice_api}/studies?external_id={study}'
+    resp = requests.get(url)
+
+    # Problem with the request
+    if resp.status_code != 200:
+        raise DataserviceException(f'Problem requesting dataservice: '
+                                   f'{url}, {resp.content}')
+    # There was more than one study returned for this accession code
+    if len(resp.json()['results']) > 1:
+        raise DataserviceException(f'More than one study found for {study}')
+    # There was no study found in the dataservice with the accession code
+    if len(resp.json()['results']) == 0:
+        raise DataserviceException(f'Could not find a study for {study}')
+
+    version = resp.json()['results'][0]['version']
+    # The study has no version registered in the dataservice
+    if not version:
+        raise DataserviceException(f'{study} has no version in dataservice')
+
+    # Need to now invoke new functions in batches to process each sample
+    dbgap_codes = read_dbgap_xml(study+'.'+version)
+    events = []
+    for row in dbgap_codes:
+        events.append(event_generator(study, row))
+    print(len(events), 'dbgap')
+    payload = {'Records': events, 'Biospecimens': [], 'GenomicFiles': []}
+    if len(payload['Records']) > 0:
+        handler(payload, DATASERVICE=dataservice_api)
+
+def event_generator(study, row):
+    """
+    Generates events for each sample in dbgap
+    """
+    ev = copy.deepcopy(record_template)
+    ev["study"]["dbgap_id"] = study
+    ev["study"]["sample_id"] = row[1]
+    ev["study"]["consent_code"] = row[0]
+    ev["study"]["consent_short_name"] = row[2]
+    return ev
+
+def biospecimen_event_generator(row):
+    """
+    Generates events for each sample in dbgap
+    """
+    ev = copy.deepcopy(bs_record_template)
+    ev['biospecimen']["study_id"] = row[0]
+    ev['biospecimen']["external_sample_id"]= row[1]
+    ev['biospecimen']["kf_id"] = row[2]
+    ev['biospecimen']["dbgap_consent_code"] = row[3]
+    ev['biospecimen']["consent_code"] = row[4]
+    ev['biospecimen']["consent_type"] = row[5]
+    ev['biospecimen']["cons_short_name"] = row[6]
+    ev['biospecimen']["visible"] = row[7]
+    ev['biospecimen']['phs_id'] = row[8]
+    return ev
+
+def gf_event_generator(row):
+    """
+    Generates events for each genomic_file
+    """
+    ev = {}
+    ev[row[0]]={"acl": row[1],
+                "visible": row[2],
+                "consent_code": [row[3]],
+                "biospecimen_id": [row[4]],
+                 "study_id": row[5],
+                 "phs_id": row[6]
+                }
+    return ev
+
+def handler(event, DATASERVICE):
     """
     Update dbgap_consent_code in biospecimen and acl's in genomic file
     from a list of lambda events. If all events are not processed before
     the lambda runs out of time, the remaining will be submitted to
     a new function
     """
-    DATASERVICE = os.environ.get('DATASERVICE', None)
+    # DATASERVICE = os.environ.get('DATASERVICE', None)
 
     if DATASERVICE is None:
         return 'no dataservice url set'
-    updater = AclUpdater(DATASERVICE, context)
+    updater = AclUpdater(DATASERVICE)
     res = {}
     while len(event['Records']) > 0:
-        if (hasattr(context, 'invoked_function_arn') and
-                context.get_remaining_time_in_millis() < 15000):
-            print('not able to complete {} records, '
-                  're-invoking the function'.format(len(event['Records'])))
-            lam = boto3.client('lambda')
-            # Invoke the lambda again with remaining records
-            response = lam.invoke(
-                FunctionName=context.invoked_function_arn,
-                InvocationType='Event',
-                Payload=str.encode(json.dumps(event))
-            )
-            # Stop processing and exit
-            break
-        else:
             try:
                 record = event['Records'].pop()
-                updater.update_acl(record)
-                res["genomic_file"] = 'processed all records'
-            except Exception:
+                bs_values = updater.update_biospecimens(record, event)
+                event['Biospecimens'].append(biospecimen_event_generator(bs_values))
+            except DataserviceException:
+                pass
+            except (TimeoutException, ValueError):
                 event['Records'].append(record)
+    while len(event['Biospecimens']) > 0:
+        try:
+            bio_record = event['Biospecimens'].pop()
+            # Do not update biospecimen if consent code is not changed
+            if ((bio_record['biospecimen']['dbgap_consent_code']
+            != bio_record['biospecimen']['consent_code']) or
+            (bio_record['biospecimen']['consent_type'] !=
+            bio_record['biospecimen']['cons_short_name'])):
+                updater.update_dbgap_consent_code(
+                    bio_record['biospecimen']['kf_id'],
+                    bio_record['biospecimen']['consent_code'],
+                    bio_record['biospecimen']['consent_type'])
+            event = updater.update_genomic_files(bio_record, event)
+        except TimeoutException:
+            event['Biospecimens'].append(bio_record)
+    # print(event)
+    while len(event['GenomicFiles']) > 0:
+        try:
+            gf_record = event['GenomicFiles'].pop()
+            updater.update_acl_genomic_file(gf_record)
+        except TimeoutException:
+            event['GenomicFiles'].append(gf_record)
     return res
 
 
 class AclUpdater:
 
-    def __init__(self, api, context):
+    def __init__(self, api):
         self.api = api
-        self.context = context
         self.external_ids = {}
         self.version = {}
 
-    def update_acl(self, record):
+    def update_biospecimens(self, record, event):
         """
         Gets the external sample id and consent code from dbgap and
         updates dbgap consent code of biospecimen and acl's of genomic files
@@ -68,34 +211,39 @@ class AclUpdater:
         external_id = record["study"]["sample_id"]
         consent_code = record["study"]["consent_code"]
         cons_short_name = record["study"]["consent_short_name"]
-        kf_id, version = self.get_study_kf_id(study_id=study)
+        study_id, version = self.get_study_kf_id(study_id=study)
         (bs_id, dbgap_cons_code,
-         consent_type, visible) = self.get_biospecimen_kf_id(
+            consent_type, visible) = self.get_biospecimen_kf_id(
             external_sample_id=external_id,
-            study_id=kf_id)
-        # if matching biospecimen is found updates the consent code
-        if not bs_id:
-            return 'Biospecimen does not exist'
+            study_id=study_id)
 
-        gf = {"acl": []}
         if not visible:
             consent_code = None
         else:
             consent_code = study+'.c' + consent_code
-            gf['acl'].extend((consent_code, study, kf_id))
+        bs_values = [study_id, external_id, bs_id, dbgap_cons_code, consent_code,
+         consent_type, cons_short_name, visible, study]
+        return bs_values
 
-        # Do not update biospecimen if consent code is not changed
-        if dbgap_cons_code != consent_code or consent_type != cons_short_name:
-            status = self.update_dbgap_consent_code(
-                biospecimen_id=bs_id,
-                consent_code=consent_code,
-                consent_short_name=cons_short_name)
-            if not status:
-                return False
-        status = self.update_acl_genomic_file(biospecimen_id=bs_id, gf=gf)
-        if not status:
-            return False
-        return True
+    def update_genomic_files(self, record, events):
+        bs_id = record['biospecimen']['kf_id']
+        study_id = record['biospecimen']["study_id"]
+        phs_id = record['biospecimen']["phs_id"]
+        consent_code = record['biospecimen']["consent_code"]
+        response = self.get_gfs_from_biospecimen(bs_id)
+        for r in response['results']:
+            gf_values = [r['kf_id'], r['acl'], r['visible'], consent_code,
+            bs_id, study_id, phs_id]
+            gf_dict = gf_event_generator(gf_values)
+            if list(dict_or_list(r['kf_id'], events)):
+                for gf in events['GenomicFiles']:
+                    for key, val in gf.items():
+                        if r['kf_id'] == key:
+                            val['consent_code'].append(consent_code)
+                            val['biospecimen_id'].append(bs_id)
+            else:
+                events['GenomicFiles'].append(gf_dict)
+        return events
 
     def get_study_kf_id(self, study_id):
         """
@@ -109,8 +257,7 @@ class AclUpdater:
             return self.external_ids[study_id], self.version[study_id]
         while retry_count > 1:
             resp = requests.get(
-                self.api+'/studies?external_id='+study_id,
-                timeout=self.context.get_remaining_time_in_millis()-14000)
+                self.api+'/studies?external_id='+study_id)
             if resp.status_code != 500:
                 break
             else:
@@ -122,6 +269,7 @@ class AclUpdater:
             self.version[study_id] = resp.json()['results'][0]['version']
             return self.external_ids[study_id], self.version[study_id]
 
+
     def get_biospecimen_kf_id(self, external_sample_id, study_id):
         """
         Gets biospecimen kf_id based on external sample id and study kf_id
@@ -130,20 +278,21 @@ class AclUpdater:
         while retry_count > 1:
             resp = requests.get(
                 self.api+'/biospecimens?study_id='+study_id +
-                '&external_sample_id='+external_sample_id,
-                timeout=self.context.get_remaining_time_in_millis()-13000)
+                '&external_sample_id='+external_sample_id)
             if resp.status_code != 500:
                 break
             else:
                 retry_count = retry_count - 1
         if resp.status_code != 200:
             raise TimeoutException
-        if len(resp.json()['results']) == 1:
+        elif len(resp.json()['results']) == 1:
             bs_id = resp.json()['results'][0]['kf_id']
             dbgap_cons_code = resp.json()['results'][0]['dbgap_consent_code']
             consent_type = resp.json()['results'][0]['consent_type']
             visible = resp.json()['results'][0]['visible']
             return bs_id, dbgap_cons_code, consent_type, visible
+        else:
+            raise DataserviceException
 
     def update_dbgap_consent_code(self, biospecimen_id,
                                   consent_code, consent_short_name):
@@ -157,8 +306,7 @@ class AclUpdater:
         while retry_count > 1:
             resp = requests.patch(
                 self.api+'/biospecimens/'+biospecimen_id,
-                json=bs,
-                timeout=self.context.get_remaining_time_in_millis()-12000)
+                json=bs)
             if resp.status_code != 500:
                 break
             else:
@@ -175,8 +323,7 @@ class AclUpdater:
         while retry_count > 1:
             resp = requests.get(
                 self.api+'/genomic-files?biospecimen_id='+biospecimen_id +
-                '&limit=100',
-                timeout=self.context.get_remaining_time_in_millis()-11000)
+                '&limit=100')
             if resp.status_code != 500:
                 break
             else:
@@ -190,28 +337,37 @@ class AclUpdater:
         else:
             return resp.json()
 
-    def update_acl_genomic_file(self, gf, biospecimen_id):
+    def update_acl_genomic_file(self, gf_record):
         """
-        Updates acl's of genomic files that are associated with biospecimen
+        Updates acl's of genomic files
         """
-        # Get the links of genomic files for that biospecimen
-        retry_count = 3
-        response = self.get_gfs_from_biospecimen(biospecimen_id)
-        for r in response['results']:
-            acl = gf
-            if not r['visible']:
+        record = list(gf_record.values())[0]
+        acl = {"acl": []}
+        if not record['visible']:
+            acl = {"acl": []}
+        else:
+            if ((len(set(record['consent_code']))>1) or
+            (set(record['consent_code']) is None)):
                 acl = {"acl": []}
-            # Do not update if acl's are as expected
-            if r['acl'] != acl['acl']:
-                while retry_count > 1:
-                    resp = requests.patch(
-                        self.api+'/genomic-files/'+r['kf_id'], json=acl,
-                        timeout=self.context
-                        .get_remaining_time_in_millis()-8000)
-                    if resp.status_code != 500:
-                        break
-                    else:
-                        retry_count = retry_count - 1
+            else:
+                acl['acl'].extend((record['consent_code'][0],
+                record['phs_id'], record['study_id']))
+        retry_count = 3
+
+        # Do not update if acl's are as expected
+        if set(record['acl']) != set(acl['acl']):
+            while retry_count > 1:
+                resp = requests.patch(
+                        self.api+'/genomic-files/'+r['kf_id'], json=acl)
+                if resp.status_code != 500:
+                    break
+                else:
+                    retry_count = retry_count - 1
                 if resp.status_code != 200:
                     raise TimeoutException
         return
+
+if __name__ == '__main__':
+    study = 'phs001410'
+    dataservice_api = 'https://kf-api-dataservice-qa.kids-first.io/'
+    map_one_study(study, dataservice_api)
--- tests/test_service.py
+++ tests/test_service.py
@@ -26,11 +26,7 @@ def test_create(event):
     os.environ['DATASERVICE'] = 'http://api.com/'
     mock = patch('service.requests')
     req = mock.start()
-
-    class Context:
-        def get_remaining_time_in_millis(self):
-            return 1600
-
+    study = "phs001168"
     def mock_get(url, *args, **kwargs):
         if '/genomic-files/' in url:
             resp = MagicMock()
@@ -109,38 +105,38 @@ def test_create(event):
     mock_resp.status_code = 201
     req.post.return_value = mock_resp
     req.patch.return_value = mock_resp
-    res = service.handler(event, Context())
+    res = service.map_one_study(study, os.environ['DATASERVICE'])
 
     assert len(res) == 1
 
-@mock_s3
-def test_out_of_time(event):
-    """ Test that a function is re-invoked when records remain """
-    os.environ['DATASERVICE_API'] = 'http://api.com/'
-    mock_r = patch('service.requests')
-    req = mock_r.start()
-
-    class Context:
-        def __init__(self):
-            self.invoked_function_arn = 'arn:aws:lambda:::function:kf-lambda'
-
-        def get_remaining_time_in_millis(self):
-            return 300
-
-    # Add a second record
-    event['Records'].append(event['Records'][0])
-
-    with patch('service.boto3.client') as mock:
-        service.handler(event, Context())
-        assert mock().invoke.call_count == 1
-
-        _, args = mock().invoke.call_args_list[0]
-        assert args['FunctionName'] == Context().invoked_function_arn
-        assert args['InvocationType'] == 'Event'
-        payload = json.loads(args['Payload'].decode('utf-8'))
-        assert  payload == {'Records': event['Records']}
-
-    mock_r.stop()
+# @mock_s3
+# def test_out_of_time(event):
+#     """ Test that a function is re-invoked when records remain """
+#     os.environ['DATASERVICE_API'] = 'http://api.com/'
+#     mock_r = patch('service.requests')
+#     req = mock_r.start()
+#
+#     class Context:
+#         def __init__(self):
+#             self.invoked_function_arn = 'arn:aws:lambda:::function:kf-lambda'
+#
+#         def get_remaining_time_in_millis(self):
+#             return 300
+#
+#     # Add a second record
+#     event['Records'].append(event['Records'][0])
+#
+#     with patch('service.boto3.client') as mock:
+#         service.handler(event, Context())
+#         assert mock().invoke.call_count == 1
+#
+#         _, args = mock().invoke.call_args_list[0]
+#         assert args['FunctionName'] == Context().invoked_function_arn
+#         assert args['InvocationType'] == 'Event'
+#         payload = json.loads(args['Payload'].decode('utf-8'))
+#         assert  payload == {'Records': event['Records']}
+#
+#     mock_r.stop()
 
 @mock_s3
 def test_get_biospecimen_id(event):
@@ -179,7 +175,7 @@ def test_get_biospecimen_id(event):
     mock_resp.status_code = 201
     req.post.return_value = mock_resp
 
-    updater = service.AclUpdater('http://api.com/', Context())
+    updater = service.AclUpdater('http://api.com/')
     updater.get_biospecimen_kf_id(external_sample_id='PA2645',
     study_id='SD_9PYZAHHE')
     assert req.get.call_count == 1
@@ -227,7 +223,7 @@ def test_update_consent_code(event):
     mock_resp.status_code = 201
     req.post.return_value = mock_resp
 
-    updater = service.AclUpdater('http://api.com/', Context())
+    updater = service.AclUpdater('http://api.com/')
     updater.update_dbgap_consent_code(biospecimen_id='BS_HFY3Y3XM',
                                   consent_code='phs001168.c1',
                                   consent_short_name='')
